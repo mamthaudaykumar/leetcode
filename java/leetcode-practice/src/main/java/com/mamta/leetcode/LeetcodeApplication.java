65ffda1b9d291abe09477cb37cea04b9dcd58852
Mamtha kumar <94455713+mamthaudaykumar@users.noreply.github.com>
2026-01-08 23:44:08 +0000

    leetcode practice repo

---
+++ java/leetcode-practice/src/main/java/com/mamta/leetcode/LeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.orgname.activemq_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqProcessorApplication.class, args);
+	}
+
+}
---
+++ java/leetcode-practice/src/test/java/com/mamta/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgname.activemq_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
