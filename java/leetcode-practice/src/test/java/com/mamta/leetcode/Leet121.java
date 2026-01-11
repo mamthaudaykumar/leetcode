498bd968d342f5d701c527752fa0ebd33abd51b8
Mamtha kumar <94455713+mamthaudaykumar@users.noreply.github.com>
2026-01-11 14:17:58 +0000

    problem 121

---
+++ java/leetcode-practice/src/test/java/com/mamta/leetcode/Leet121.java
@@ -0,0 +1,36 @@
+package com.mamta.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leet121 {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int buyPrice = prices[0];
+        int profit = 0;
+        for(int i=1; i<prices.length;i++) {
+
+            if(profit < prices[i]-buyPrice ) {
+                profit = prices[i]-buyPrice;
+            }
+            if(prices[i] < buyPrice ) {
+                buyPrice = prices[i];
+            }
+
+
+        }
+        return profit;
+
+    }
+
+    @Test
+    public void solution() {
+        int[] prices = {7,1,5,3,6,4};
+
+      System.out.println(maxProfit(prices));
+
+    }
+}
--- java/leetcode-practice/src/test/java/com/mamta/leetcode/Leet26.java
+++ java/leetcode-practice/src/test/java/com/mamta/leetcode/Leet26.java
@@ -1,4 +1,32 @@
 package com.mamta.leetcode;
 
+import org.junit.jupiter.api.Test;
+
 public class Leet26 {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    @Test
+    public void solution() {
+        int[] nums = {1, 1, 2};
+        int newLength = removeDuplicates(nums);
+
+        System.out.println("New length: " + newLength);
+        System.out.print("Modified array: ");
+        for (int k = 0; k < newLength; k++) {
+            System.out.print(nums[k] + " ");
+        }
+    }
 }
